5b0d638cbc44e3484906300abaf6e287102311c8
Anselmo Vasconcelos <anselmovas@gmail.com>
2023-05-26 13:56:37 -0300

    Encontro Remoto 2 - Atividades

---
+++ UC13/04C/Atividade_04C.sql
@@ -0,0 +1,66 @@
+--ATIVIDADE 4C
+--CRIAÇÃO DA BASE DE DADOS
+CREATE DATABASE RpgResposta;
+
+--COLOCAR BASE DE DADOS EM USO
+USE RpgResposta;
+
+--CRIAÇÃO DAS TABELAS NO BANCO
+CREATE TABLE Usuarios
+(
+UsuarioId INT PRIMARY KEY IDENTITY,
+Email VARCHAR(150) UNIQUE NOT NULL,
+Senha VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE Classes
+(
+	ClasseId INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(150) UNIQUE NOT NULL,
+	Descricao VARCHAR(255)
+);
+
+CREATE TABLE Personagens
+(
+	PersonagenmId INT PRIMARY KEY IDENTITY,
+	NomePersonagem VARCHAR(10) UNIQUE NOT NULL,
+	UsuarioId INT UNIQUE FOREIGN KEY REFERENCES Usuarios(UsuarioId),
+	ClasseId INT FOREIGN KEY REFERENCES Classes(ClasseId)
+);
+
+CREATE TABLE Habilidades
+(
+	HabilidadeId INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(150) UNIQUE NOT NULL,
+);
+
+CREATE TABLE ClassesHabilidades
+(
+	ClasseId INT FOREIGN KEY REFERENCES Classes(ClasseId),
+	HabilidadeId INT FOREIGN KEY REFERENCES Habilidades(HabilidadeId)
+);
+
+--INSERÇÃO DE DADOS NO BANCO DE DADOS
+INSERT INTO Usuarios VALUES ('Danos', 'Senha1234');
+INSERT INTO Usuarios VALUES ('Bora', 'Senha1234');
+INSERT INTO Usuarios VALUES ('Teste', 'Senha1234');
+INSERT INTO Classes VALUES ('Monge', 'Descrição do Monge');
+INSERT INTO Habilidades VALUES ('Recuperar Vida');
+INSERT INTO Personagens (NomePersonagem, UsuarioId, ClasseId) VALUES ('DeuBug',1,1);
+INSERT INTO ClassesHabilidades(ClasseId,HabilidadeId) VALUES (1,1);
+
+--SELEÇÃO DE DADOS DO BANCO
+SELECT * FROM Usuarios;
+SELECT * FROM Classes;
+SELECT * FROM Habilidades;
+SELECT * FROM Personagens;
+SELECT * FROM ClassesHabilidades;
+
+--BUSCAR EMAIL INDIVIDUAL
+SELECT Email, Senha FROM Usuarios WHERE Email = 'Teste';
+
+-- ALTERAÇÃO NOS DADOS
+UPDATE Usuarios SET Email = 'Teste Alterado'	WHERE UsuarioId = 2;
+
+--EXCLUSÃO DE DADOS
+DELETE FROM Usuarios	WHERE UsuarioId=2;
