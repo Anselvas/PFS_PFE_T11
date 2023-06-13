14572feba2c0b359ae0abd6675c9da9f11346985
Anselmo Vasconcelos <anselmovas@gmail.com>
2023-06-13 18:01:07 -0300

    ENCONTRO REMOTO2 de API

---
+++ UC14/04D-Atividade_4/Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,32 @@
+﻿
+using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext:DbContext
+    {
+       public ChapterContext()
+        {
+
+        }
+
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options) 
+        { 
+
+        } 
+        //vamos utilizar esse método para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sitaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = LAPTOP-BFCJC1OV\\SQLEXPRESS ; Initial catalog = Chapter; Integrated Security = True");
+            }
+        }
+        //dbset respresenta as entidades que serão utilizadas nas operações de leitura, criação, atualização e exclusão
+         public DbSet<Livro> Livros  { get; set; }
+
+    }
+}
